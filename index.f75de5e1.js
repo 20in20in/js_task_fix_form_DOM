40500d77dd195de01243278d15fecf09c93aef6b
Igor Novikov <novikov.igor.20@gmail.com>
2024-06-19 19:22:26 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const allInput = [
+    ...document.querySelectorAll("form input")
+];
+allInput.forEach((el)=>{
+    const label = document.createElement("label");
+    label.classList.add("field-label");
+    if (el.id) label.setAttribute("for", el.id);
+    const fieldName = formatFiledName(el.name);
+    label.textContent = fieldName;
+    el.placeholder = fieldName;
+    el.parentNode.insertBefore(label, el);
+});
+function formatFiledName(str) {
+    const withSpaces = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+    return withSpaces.charAt(0).toUpperCase() + withSpaces.slice(1);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
